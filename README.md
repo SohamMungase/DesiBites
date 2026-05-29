3a234e554ddfda2dec2f18d256c6193268a17fae
Soham Mungase <sohammungase24@gmail.com>
2026-05-29 18:48:50 +0530

    created cards with API integration

--- README.md
+++ README.md
@@ -5,4 +5,26 @@
     -logo
     -nav item
 
+#Day - 04
+
+React Card Component with API Data
+Overview
+
+This project demonstrates how to create reusable React card components and dynamically populate them using data fetched from an API.
+
+Features:
+
+Built using React
+Reusable Card Component
+Dynamic rendering using API data
+Clean and responsive UI
+Component-based architecture
+Technologies Used
+React.js
+JavaScript (ES6+)
+HTML5
+CSS3
+API Integration
+
+
     
--- app.js
+++ app.js
@@ -1,24 +1,236 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const Header = ()=>{
-    return ( <div className="header">
-    <div className="logo-container">
-        <img className="logo" src="https://img.magnific.com/premium-vector/food-logo-with-smile-spoon-fork-delicious-food-design-illustration-tongue-saliva_207371-61.jpg?w=2000"/>
-    </div>
+const reslist = [
+  {
+    "@type": "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
+    info: {
+      id: "21001",
+      name: "Pizza Hut",
+      cloudinaryImageId:
+        "RX_THUMBNAIL/IMAGES/VENDOR/2026/2/12/dd196a56-86a8-409b-8e29-df0dbbfd76fc_21001.JPG",
+      locality: "F.C. Road",
+      areaName: "Shivajinagar",
+      costForTwo: "₹350 for two",
+      cuisines: ["Pizzas"],
+      avgRating: 4,
+      parentId: "721",
+      avgRatingString: "4.0",
+      totalRatingsString: "13K+",
+      sla: {
+        deliveryTime: 24,
+        lastMileTravel: 1.5,
+        serviceability: "SERVICEABLE",
+        slaString: "20-25 mins",
+        lastMileTravelString: "1.5 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2026-05-30 05:00:00",
+        opened: true,
+      },
+      badges: {},
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "FREE ITEM",
+      },
+      orderabilityCommunication: {
+        title: {},
+        subTitle: {},
+        message: {},
+        customIcon: {},
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+      externalRatings: {
+        aggregatedRating: {
+          rating: "4.4",
+          ratingCount: "5.6K+",
+        },
+        source: "GOOGLE",
+        sourceIconImageId: "v1704440323/google_ratings/rating_google_tag",
+      },
+      ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY",
+    },
+    analytics: {
+      context: "seo-data-da0e3b8e-5267-4056-bf6e-1a4c2b26c009",
+    },
+    cta: {
+      link: "https://www.swiggy.com/city/pune/pizza-hut-f-c-road-shivajinagar-rest21001",
+      text: "RESTAURANT_MENU",
+      type: "WEBLINK",
+    },
+    widgetId: "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo",
+  },
+   {
+    "@type": "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
+    info: {
+      id: "21003",
+      name: "Pizza Hut",
+      cloudinaryImageId:
+        "RX_THUMBNAIL/IMAGES/VENDOR/2026/2/12/dd196a56-86a8-409b-8e29-df0dbbfd76fc_21001.JPG",
+      locality: "F.C. Road",
+      areaName: "Shivajinagar",
+      costForTwo: "₹350 for two",
+      cuisines: ["Pizzas"],
+      avgRating: 4,
+      parentId: "721",
+      avgRatingString: "4.0",
+      totalRatingsString: "13K+",
+      sla: {
+        deliveryTime: 24,
+        lastMileTravel: 1.5,
+        serviceability: "SERVICEABLE",
+        slaString: "20-25 mins",
+        lastMileTravelString: "1.5 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2026-05-30 05:00:00",
+        opened: true,
+      },
+      badges: {},
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "FREE ITEM",
+      },
+      orderabilityCommunication: {
+        title: {},
+        subTitle: {},
+        message: {},
+        customIcon: {},
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+      externalRatings: {
+        aggregatedRating: {
+          rating: "4.4",
+          ratingCount: "5.6K+",
+        },
+        source: "GOOGLE",
+        sourceIconImageId: "v1704440323/google_ratings/rating_google_tag",
+      },
+      ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY",
+    },
+    analytics: {
+      context: "seo-data-da0e3b8e-5267-4056-bf6e-1a4c2b26c009",
+    },
+    cta: {
+      link: "https://www.swiggy.com/city/pune/pizza-hut-f-c-road-shivajinagar-rest21001",
+      text: "RESTAURANT_MENU",
+      type: "WEBLINK",
+    },
+    widgetId: "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo",
+  },
+  
+  
+];
 
-    <div className="nav-items">
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://img.magnific.com/premium-vector/food-logo-with-smile-spoon-fork-delicious-food-design-illustration-tongue-saliva_207371-61.jpg?w=2000"
+        />
+      </div>
+
+      <div className="nav-items">
         <ul>
-            <li>Home</li>
-            <li>About us</li>
-            <li>Contact us</li>
-            <li>Cart</li>
+          <li>Home</li>
+          <li>About us</li>
+          <li>Contact us</li>
+          <li>Cart</li>
         </ul>
+      </div>
     </div>
+  );
+};
+
+const Restaurantcard = (props) => {
+  const {reslist} = props
+  const {name,cuisines,avgRating,sla} = reslist.info;
+  return (
+    <div className="res-card">
+      <img
+        className="res-logo"
+        src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2026/2/12/dd196a56-86a8-409b-8e29-df0dbbfd76fc_21001.JPG"
+      />
+      
+      <h1>{name}</h1>
+      <h4>{cuisines.join(", ")}</h4>
+      <h4>{avgRating} Stars</h4>
+      <h4>{sla.deliveryTime}Minutes</h4>
+      
     </div>
-    )
-}
+  );
+};
 
-const root = ReactDOM.createRoot(document.getElementById("root"))
-root.render(<Header/>)
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">Search</div>
+      <div className="res-container">
+        {/* <Restaurantcard reslist={reslist[0]} /> */}
 
+        {
+          reslist.map((restaurant)=>{
+            return <Restaurantcard key = {restaurant.info.id} reslist = {restaurant}/>
+          })
+        }
+      </div>
+    </div>
+  );
+};
+
+const Footer = () => {
+  return <div>Footer</div>;
+};
+
+const Applayout = () => {
+  return (
+    <div>
+      <Header />
+      <Body />
+      <Footer />
+    </div>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Applayout />);
